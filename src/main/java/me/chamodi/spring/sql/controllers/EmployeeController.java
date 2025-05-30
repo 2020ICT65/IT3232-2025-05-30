1831293b4a971d484285e0ee822573ba3451033b
Chamodi Indrejith <chamodiindrejith14@gmail.com>
2025-05-30 10:26:52 +0530

    fix : EmployeeController

--- src/main/java/me/chamodi/spring/sql/controllers/EmployeeController.java
+++ src/main/java/me/chamodi/spring/sql/controllers/EmployeeController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import me.chamodi.spring.sql.models.Department;
 import me.chamodi.spring.sql.models.Employee;
 import me.chamodi.spring.sql.services.EmployeeService;
 
@@ -24,5 +26,13 @@ public class EmployeeController {
         return new ResponseEntity<List<Employee>>(service.getEmployeesInSalaryRange(), HttpStatus.OK);
     }
 
-    
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Employee>>getEmployeesInDepartment(@PathVariable("id") String id){
+        return new ResponseEntity<List<Employee>>(service.getEmployeesInDepartment(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/youngestFemale")
+    public ResponseEntity <Employee> getYoungestFemale(){
+        return new ResponseEntity <Employee>(service.getYoungestFemale(), HttpStatus.OK);
+    }
 }
